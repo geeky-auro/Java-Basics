e952395637b4ce14c1cccd391b694e7277faefbb
Auro Saswat Raj <83534307+geeky-auro@users.noreply.github.com>
2023-02-19 22:49:55 +0530

    Assignment 4 Q1

---
+++ Assignment-4/question1.java
@@ -0,0 +1,9 @@
+// Write a Java program to print your name, roll no, section and branch in separate lines.
+public class question1{
+    public static void main(String[] args) {
+        System.out.println("Name: AURO");
+        System.out.println("Roll No. 22057020");
+        System.out.println("Section: CSE-02");
+        System.out.println("Branch: CSE");
+    }
+}
