70dedc921f3f0d139ad9a3cdf4a1bf75f48462d8
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-02-27 17:16:14 +0530

    Question 3

---
+++ Assignment-7/question3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class question3{
+public static void main(String args[]){
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter No. of elements to be entered");
+	int n=sc.nextInt();
+	int arr[]=new int[n];
+	for(int i=0;i<n;i++){
+	arr[i]=sc.nextInt();
+	}
+	for(int i=0;i<n-1;i++){
+	for(int j=0;j<n-i-1;j++){
+	if(arr[i]>arr[j+1]){
+	int temp=arr[i];
+	arr[i]=arr[j];
+	arr[j]=temp;
+	}
+	}
+	}
+	
+	for(int x:arr){
+	System.out.println(x);
+	}
+		
+}
+}
