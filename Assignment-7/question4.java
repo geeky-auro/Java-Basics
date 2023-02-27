f6bef56bafc56236c4e7ac8c4385bb0d1cdd6c6e
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-02-27 17:36:54 +0530

    question 4 updated

---
+++ Assignment-7/question4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+/**
+ * question4
+ */
+public class question4 {
+    static int count=0;
+    question4(){
+        count++;
+    }
+    public static void main(String[] args) {
+        question4 q1=new question4();
+        question4 q2=new question4();
+        question4 q3=new question4();
+        question4 q4=new question4();
+        question4 q5=new question4();
+        System.out.println("Total Object Count is "+count);
+    }
+}
