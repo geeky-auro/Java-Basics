955645f8ce93c62eab1d05f68479e17f246e6cc8
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-02-27 17:57:50 +0530

    question3

--- Assignment-7/question3.java
+++ Assignment-7/question3.java
@@ -10,10 +10,10 @@ public static void main(String args[]){
 	}
 	for(int i=0;i<n-1;i++){
 	for(int j=0;j<n-i-1;j++){
-	if(arr[i]>arr[j+1]){
-	int temp=arr[i];
-	arr[i]=arr[j];
-	arr[j]=temp;
+	if(arr[j]>arr[j+1]){
+	int temp=arr[j];
+	arr[j]=arr[j+1];
+	arr[j+1]=temp;
 	}
 	}
 	}
