fb58322b16f164f294337a1ae9f6ea58b0351549
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-03-12 21:09:34 +0530

    Usage of Math Classes

--- Class Work/label.java
+++ Class Work/label.java
@@ -7,5 +7,14 @@ public class label {
             }
             System.out.println("......."+i );
          }  
+         System.out.println(Math.cbrt(8));
+         System.out.println(Math.ceil(8.5));
+         System.out.println(Math.ceil(8.7));
+         System.out.println(Math.ceil(8.1));
+         System.out.println(Math.floor(8.5));
+         System.out.println(Math.floor(8.7));
+         System.out.println(Math.floor(8.1));
+         System.out.println(Math.log10(10));
+         System.out.println(Math.random());
     }
 }
