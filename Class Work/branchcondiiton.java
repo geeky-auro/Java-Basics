f6a4a64b5ed568cf2079ee35e0490d5bd660b2a6
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-03-12 20:52:31 +0530

    Some Modifications

---
+++ Class Work/branchcondiiton.java
@@ -0,0 +1,11 @@
+public class branchcondiiton {
+    
+    public static void main(String[] args) {
+        
+        System.out.println(20);
+        
+        for(int i=0;i<10;i++){
+
+        }
+    }
+}
---
+++ Class Work/quiz.java
@@ -0,0 +1,24 @@
+public class quiz {
+    public static void main(String[] args) {
+        
+    }
+}
+
+abstract class Innerquiz_1 {
+
+    
+}
+
+class a{
+    String s="AAA";
+    /**
+     * Innerquiz
+     */
+    public class Innerquiz {
+        String s="BBB";
+        void methodB(){
+            System.out.println(s);
+        }
+        
+    }
+}
