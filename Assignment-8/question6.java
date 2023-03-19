6f0d5febc4fa08d8973c84b3d35c91e1acc85ed2
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-03-19 19:54:49 +0530

    Question6 Assignment 8

--- Assignment-8/question6.java
+++ Assignment-8/question6.java
@@ -1,3 +1,30 @@
 public class question6 {
-    
+    public static void main(String[] args) {
+        question6 q6=new question6();
+        question6.Car c=q6.new Car("Maruti", "Toyata", "G6");
+        question6.Car.Engine e=c.new Engine("Bigger");
+        e.display();
+    }
+    class Car{
+        String name;
+        String brand;
+        String model;
+        Car(String name,String brand,String model){
+            this.name=name;
+            this.brand=brand;
+            this.model=model;
+        }
+        class Engine{
+            String engine;
+            Engine(String engine){
+                this.engine=engine;
+            }
+            void display(){
+                System.out.println("Car Name:"+name);
+                System.out.println("Brand:"+brand);
+                System.out.println("Model:"+model);
+                System.out.println("Engine Type:"+engine);
+            }
+        }
+    }
 }
