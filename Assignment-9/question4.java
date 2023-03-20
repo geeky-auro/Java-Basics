477a53333ccb7959ee29413caec269c6f0fef0eb
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-03-20 17:22:20 +0530

    question4 Assignment 9

---
+++ Assignment-9/question4.java
@@ -0,0 +1,27 @@
+// package Assignment-9;
+
+public class question4 {
+   public static void main(String[] args) {
+    
+   } 
+}
+
+interface Motor{
+    int capacity=20;
+    void run();
+    void consume();
+}
+
+class WashingMachine implements Motor{
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        System.out.println("Washing Machine is Running ");
+    }
+
+    @Override
+    public void consume() {
+        // TODO Auto-generated method stub
+        System.out.println("Washing Machine can consume a Maximum Load of "+ capacity +" Kg");
+    }
+}
