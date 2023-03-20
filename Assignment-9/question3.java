e47d91f1ddb1b32d9e1efc318329b35470b3a834
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-03-20 17:18:39 +0530

    Question 3 Assignmnet 9

--- Assignment-9/question3.java
+++ Assignment-9/question3.java
@@ -1,7 +1,22 @@
 // package Assignment-9;
 
 public class question3 {
-    
+    public static void main(String[] args) {
+        RedmiPhone r=new RedmiPhone();
+        r.displayCamera();
+        r.displayTouchScreen();
+        r.fingerPrint();
+
+        SamsungPhone s=new SamsungPhone();
+        s.displayCamera();
+        s.displayTouchScreen();
+        s.fingerPrint();
+
+        NokiaPhone n=new NokiaPhone();
+        n.displayCamera();;
+        n.displayTouchScreen();
+        n.turboCharge();
+    }
 }
 
 abstract class MobilePhone{
@@ -10,5 +25,67 @@ abstract class MobilePhone{
     }
     abstract void displayCamera();
     abstract void displayTouchScreen();
-    
+
 }  
+
+interface AdditionalProperties{
+    void fingerPrint();
+}
+
+class RedmiPhone extends MobilePhone implements AdditionalProperties{
+    @Override
+    void displayCamera() {
+        // TODO Auto-generated method stub
+        System.out.println("Redmi phone Camera");
+    }
+
+    @Override
+    void displayTouchScreen() {
+        // TODO Auto-generated method stub
+        System.out.println("Redmi Phone Touch Screen");
+    }
+
+    @Override
+    public void fingerPrint() {
+        // TODO Auto-generated method stub
+        System.out.println("Redmi phone Fingerprint");    
+    }
+}
+
+class SamsungPhone extends MobilePhone implements AdditionalProperties{
+    @Override
+    void displayCamera() {
+        // TODO Auto-generated method stub
+        System.out.println("Samsung Camera");
+    }
+
+    @Override
+    void displayTouchScreen() {
+        // TODO Auto-generated method stub
+        System.out.println("Samsung Touch Screen");
+    }
+
+    @Override
+    public void fingerPrint() {
+        // TODO Auto-generated method stub
+        System.out.println("Samsung Touch Screen");
+    }
+}
+
+class NokiaPhone extends MobilePhone{
+    @Override
+    void displayCamera() {
+        // TODO Auto-generated method stub
+        System.out.println("Nokia Phone Camera");
+    }
+
+    @Override
+    void displayTouchScreen() {
+        // TODO Auto-generated method stub
+        System.out.println("Nokia Phone Touch Screen");
+    }
+
+    void turboCharge(){
+        System.out.println("Nokia Phone Turbo Charge");
+    }
+}
