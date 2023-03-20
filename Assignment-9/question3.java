71b6a5c6c90c7b45f66eab04e0bb89a6381b324e
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-03-20 17:04:46 +0530

    question2

---
+++ Assignment-9/question3.java
@@ -0,0 +1,14 @@
+// package Assignment-9;
+
+public class question3 {
+    
+}
+
+abstract class MobilePhone{
+    void voiceCall(){
+
+    }
+    abstract void displayCamera();
+    abstract void displayTouchScreen();
+    
+}  
