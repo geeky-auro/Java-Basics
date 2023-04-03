71e4ddd9ec70d39afd51e2b675b67aa78ddba3c3
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-04-03 17:12:17 +0530

    Question1 Assignment 11 Exception Handling

---
+++ Assignment-11/question1.java
@@ -0,0 +1,37 @@
+//user-defined exception in java
+import java.util.*;
+
+//custom exception to validate login credentials
+class NegativeNumberException extends Exception {
+    //member variable to store our custom message
+    String msg;
+    NegativeNumberException(String msg) {
+        //passing the parameter to the super class constructor
+        super(msg);
+        this.msg=msg;
+    }
+    
+    //overriding with our custom message
+    @Override
+    public String toString() {
+        return msg;
+    }
+}
+
+public class question1 {
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+	    try {
+	        if(n<0){
+	            throw new NegativeNumberException("no such user with username - "+n);
+	        }else{
+                System.out.println("No Exception and Number is "+n);
+            }
+	    }
+	    catch(NegativeNumberException ex) {
+	        //calls override toString() method
+	        System.out.println(ex);
+	    }
+	}
+}
