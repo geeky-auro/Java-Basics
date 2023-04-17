25b68c1ac07b7f1485d10c71bf9d1412dcfaeae8
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-04-17 17:48:20 +0530

    Assignment 13 - question1

---
+++ Assignment-13/question1.java
@@ -0,0 +1,18 @@
+// package Applet Programming;
+import java.awt.*;
+import java.applet.*;
+
+/*
+<html>
+<applet code = "question1.class" width = 500 height = 300>
+</applet>
+</html>
+*/
+
+public class question1 extends Applet{
+    public void paint(Graphics g){
+        g.drawString("Roll : 22057020",150,150);
+        g.drawString("Name : AURO ",200,200);  
+        g.drawString("Branch : CSE",250,250);    
+    }
+}
