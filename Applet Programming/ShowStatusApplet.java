bd364f4c53f95f09c647c706e82f51bdc0d993b3
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-04-17 16:52:21 +0530

    Applet Programming

--- Applet Programming/ShowStatusApplet.java
+++ Applet Programming/ShowStatusApplet.java
@@ -1,6 +1,12 @@
 // package Applet Programming;
 import java.awt.*;
 import java.applet.*;
+
+/*
+<applet code = "ShowStatusApplet" width = 500 height = 300>
+</applet>
+*/
+
 public class ShowStatusApplet extends Applet{
     public void paint(Graphics g){
         g.drawString("welcome",150,150);  
